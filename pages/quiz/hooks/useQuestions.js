76457b6a19ab6eaa5193d985679c49e3c80ba488
Janne Peippo <janne.peippo@poutapilvi.fi>
2021-12-11 13:31:50 +0200

    Add question fetching hook

---
+++ pages/quiz/hooks/useQuestions.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+const useQuestions = (difficulty = "medium") => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [questions, setQuestions] = useState();
+
+	useEffect(() => {
+		const shuffleArray = (array) => {
+			for (let i = array.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[array[i], array[j]] = [array[j], array[i]];
+			}
+		};
+
+		const shapeQuestions = (questions) => {
+			const shapedQuestions = questions.map((question) => {
+				const answers = [
+					question.correct_answer,
+					...question.incorrect_answers,
+				];
+				shuffleArray(answers);
+
+				const correctAnswerIndex = answers.indexOf(question.correct_answer);
+
+				return {
+					question: question.question,
+					answers,
+					correctAnswer: correctAnswerIndex,
+				};
+			});
+
+			return shapedQuestions;
+		};
+
+		const fetchQuestions = async () => {
+			setIsLoading(true);
+
+			try {
+				const response = await fetch(
+					`https://opentdb.com/api.php?amount=5&category=15&difficulty=${difficulty}&type=multiple`
+				);
+				const questions = await response.json();
+				const shapedQuestions = shapeQuestions(questions.results);
+				setQuestions(shapedQuestions);
+				console.log(shapedQuestions);
+			} catch (error) {
+				setIsError(true);
+				console.log(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchQuestions();
+	}, []);
+
+	return [isLoading, isError, questions];
+};
+
+export default useQuestions;
