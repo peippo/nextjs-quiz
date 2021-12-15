855c133cb7c9f4689886cda8c826e8c7af685d1a
Janne Peippo <janne.peippo@poutapilvi.fi>
2021-12-15 18:49:49 +0200

    Add landing page description

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,21 @@ export default function Home() {
 	return (
 		<Layout>
 			<main>
+				<p className={styles.description}>
+					Five questions, get the majority right and you gain coins,
+					otherwise you lose them. Medium and hard difficulties have
+					modifiers that make your gains - and losses - bigger!
+				</p>
+
+				<p className={styles.description}>
+					<strong>
+						<a className={styles.logLink} href="/api/auth/login">
+							Login
+						</a>{" "}
+						to keep track of your stats and reach the top of the high
+						score list!
+					</strong>
+				</p>
 				<h1>Select difficulty to begin</h1>
 				<StartPrompt />
 			</main>
